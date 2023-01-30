ecb24e3bac0bf547cbaee35317faf6455b0fad75
Dominos0321 <dsch792@gmail.com>
2023-01-30 15:13:01 +0100

    Zgodne?

--- app/src/main/java/com/ft/zadanielekcja/MainActivity.kt
+++ app/src/main/java/com/ft/zadanielekcja/MainActivity.kt
@@ -2,10 +2,13 @@ package com.ft.zadanielekcja
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckedTextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
