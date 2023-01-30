87dafb308e7b77ae8147b817ca92d1bd6cc3e867
Dominos0321 <dsch792@gmail.com>
2023-01-30 15:46:42 +0100

    Zgodne dziala

--- app/src/main/java/com/ft/zadanielekcja/MainActivity.kt
+++ app/src/main/java/com/ft/zadanielekcja/MainActivity.kt
@@ -2,7 +2,9 @@ package com.ft.zadanielekcja
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.CheckedTextView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +12,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            var x=findViewById<TextView>(R.id.editTextTextPassword3).text.toString()
+            var y=findViewById<TextView>(R.id.editTextTextPassword4).text.toString()
+
+
+            if (x==y){
+                findViewById<CheckedTextView>(R.id.zgodne).isChecked=true
+
+            }
+
+            if (x!=y){
+                findViewById<CheckedTextView>(R.id.zgodne).isChecked=false
+            }
+
+        }
     }
 }
